956dc06632be1b3f16ef780af62521e5fa6434c1
carlo <cgferrari@gmail.com>
2023-07-19 18:08:15 -0300

    logramos que entrene una epoca en la nube y escriba el resultado en el bucket

--- train/covers_prototype_train.py
+++ train/covers_prototype_train.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 import shutil
-#BUCKET_ROOT = "/gcs/covers_image_dataset"
+BUCKET_ROOT = "/gcs/covers_image_dataset"
 
 
 model = YOLO("yolov8n-cls.pt")
 
-dataset_path = "images"
+dataset_path = BUCKET_ROOT + "/"+"images"
 
-number_of_epochs = 1
+number_of_epochs = 100
 
 model.train(data=dataset_path, epochs=number_of_epochs)
 
