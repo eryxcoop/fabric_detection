8d9420b33563a9958eb9d663d2f1155acf8a3822
carlo <cgferrari@gmail.com>
2023-07-20 18:08:33 -0300

    cambie la imagen base por una de vertex ai

--- train/covers_prototype_train.py
+++ train/covers_prototype_train.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
+from datetime import datetime
 import shutil
+import torch
 BUCKET_ROOT = "/gcs/covers_image_dataset"
 
+if torch.cuda.is_available():
+    print("Cuda is available :)")
 
 model = YOLO("yolov8n-cls.pt")
 
@@ -13,5 +17,7 @@ model.train(data=dataset_path, epochs=number_of_epochs)
 
 
 source = "runs"
-destination = "/gcs/covers_image_dataset"
+
+date_timestamp = datetime.now().strftime("%d%m%Y_%H%M")
+destination = "/gcs/covers_image_dataset/results/" + date_timestamp
 shutil.move(source, destination)
